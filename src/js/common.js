b3de36e6b7a0e6a0f6f1b20bdf7b02578e611136
approxua <kotenko1995@gmial.com>
2018-03-13 09:39:24 +0200

    add services slider

--- src/js/common.js
+++ src/js/common.js
@@ -6,6 +6,15 @@ $(document).ready(function(){
         prevArrow: '<i class="icon-left-open arrow">',
         nextArrow: '<i class="icon-right-open arrow">'
     });
+    $('.serv-slider').slick({
+        infinite: true,
+        vertical: true,
+        verticalSwiping: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: false,
+        dots: true
+    });
 });
 
 
